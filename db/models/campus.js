ce3f6be36ef30f9d10a091148626c2515f5249dc
Olivia Oddo <olivia.oddo@gmail.com>
2017-07-07 10:15:24 -0400

    in progress solution

--- db/models/campus.js
+++ db/models/campus.js
@@ -1,9 +1,16 @@
 'use strict';
 var Sequelize = require('sequelize')
 var db = require('../index.js')
-
+const {User} = require('./index');
 
 module.exports = db.define('campus', {
   name: Sequelize.STRING,
   image: Sequelize.STRING
-});
+}, {
+  hooks: {
+    beforeDestroy: function(campus){
+      User.update({campusId: null}, {where: {campusId: campus.id}});
+        }
+    }
+}
+);
--- server/api.js
+++ server/api.js
@@ -7,8 +7,9 @@ const {User, Campus} = require('../db/models');
 // If you aren't getting to this object, but rather the index.html (something with a joke) your path is wrong.
 	// I know this because we automatically send index.html for all requests that don't make sense in our backend.
 	// Ideally you would have something to handle this, so if you have time try that out!
-api.get('/hello', (req, res) => res.send({hello: 'world'}))
 
+//^ try handling errors in start.js
+// try putting campuses and users in separate files
 
 //get all of the campuses
 api.get('/campuses', (req, res, next) => {
@@ -17,9 +18,9 @@ api.get('/campuses', (req, res, next) => {
         if(!campuses) res.sendStatus(400);
         else res.json(campuses);
     })
-    .catch(err=>{
+    .catch(err => {
         res.sendStatus(err.status);
-    })
+    });
 });
 
 //get a single campus
@@ -29,9 +30,9 @@ api.get('/campus/:id', (req, res, next) => {
         if(!campus) res.sendStatus(404);
         else res.json(campus);
     })
-    .catch(err=>{
+    .catch(err=> {
         res.sendStatus(err.status);
-    })
+    });
 });
 
 
@@ -42,9 +43,9 @@ api.get('/students', (req, res, next) => {
         if(!users) res.sendStatus(404);
         else res.json(users);
     })
-    .catch(err=>{
+    .catch(err=> {
         res.sendStatus(err.status);
-    })
+    });
 });
 
 //get a single student
@@ -54,58 +55,71 @@ api.get('/student/:id', (req, res, next) => {
         if(!user) res.sendStatus(404);
         else res.json(user);
     })
-    .catch(err=>{
+    .catch(err=> {
         res.sendStatus(err.status);
-    })
+    });
 });
 
 
 //create a new campus
 api.post('/campus', (req, res, next) => {
     Campus.create({name: req.body.name, image: req.body.image})
-    .then(campus =>{
+    .then(campus => {
         res.sendStatus(200);
     })
-    .catch(err =>{
+    .catch(err => {
         res.send(err.message);
-    })
+    });
 });
 
 //create a new student
 api.post('/student', (req, res, next) => {
     Promise.all([
         Campus.findById(req.body.id),
-        User.create({name: req.body.name, email: req.body.email})
+        User.create({first_name: req.body.firstName, last_name: req.body.lastName, email: req.body.email})
     ])
     .spread((campus, user) => {
         user.setCampus(campus)
         res.sendStatus(200);
     })
-    .catch(err =>{
+    .catch(err => {
         res.send(err.message);
-    })
+    });
 });
 
 //update a student
+api.put('/student/:id', (req, res) => {
+
+});
 
 //update a campus
+api.put('/campus/:id', (req, res) => {
 
+});
 
 //delete a student
 api.delete('/student/:id', (req, res, next) => {
     User.destroy({where: {id: req.params.id}})
-    .then( () =>{
+    .then( () => {
         res.sendStatus(202)
     })
-    .catch( err =>{
+    .catch( err => {
         res.status(err.status).send(err.message);
-    })
-})
+    });
+});
 
 //delete a campus
+api.delete('/campus/:id', (req, res, next) =>{
+    Campus.destroy({where: {id: req.params.id}})
+    .then( () => {
+        res.sendStatus(202);
+    })
+    .catch( err => {
+        res.status(err.status).send(err.message);
+    });
+});
 
-//delete all of the students before or check to make sure there are no students
 // when adding a photo, beforeCreate a campus image add a path to public
 
 
-module.exports = api
+module.exports = api;
